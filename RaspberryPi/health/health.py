65a46648e169251bc1760a62fbc04073f4e994f4
jasonbrownza <jasonbza@protonmail.com>
2021-01-10 22:35:08 +0200

    Initial Commit

--- RaspberryPi/health/health.py
+++ RaspberryPi/health/health.py
@@ -4,7 +4,7 @@ import os
 import threading
 import time
 import traceback
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 import ssl
 
 # ======================================================================================================================================
@@ -22,21 +22,6 @@ MQTT_PASS = "REPLACE_WITH_YOUR_MQTT_PASSWORD" # Your mqtt password
 
 MQTT_USER_PASS = dict(username=MQTT_USER, password=MQTT_PASS)
 
-def every(delay, task):
-  next_time = time.time() + delay
-  while True:
-    time.sleep(max(0, next_time - time.time()))
-    try:
-      task()
-    except Exception:
-      err = traceback.print_exc()
-      #traceback.print_exc()
-      # in production code you might want to have this instead of course:
-      # logger.exception("Problem while executing repetitive task.")
-    # skip tasks if we are behind schedule:
-    next_time += (time.time() - next_time) // delay * delay + delay
-
-
 # Return RAM information (unit=kb) in a list
 # Index 0: total RAM
 # Index 1: used RAM
@@ -53,48 +38,64 @@ def getRAMinfo():
 
 def cpuTemp():
   cputemp = os.popen("cat /sys/class/thermal/thermal_zone0/temp").read()
-  topic = MQTT_CLIENT_CODE + "/picputemp/sensor/update"
   payload = '{"value":' + str(int(int(cputemp)/1000)).strip() + '}'
   payload = payload.replace('\r', '').replace('\n', '')
-  doPublish(topic, payload)
+  return payload
 
 
 def diskUsed():
   usedspace = os.popen("df --output=pcent | awk -F'%' 'NR==2{print $1}'").read()
-  topic = MQTT_CLIENT_CODE + "/pidiskused/sensor/update"
   payload = '{"value":' + str(usedspace).strip() + '}'
   payload = payload.replace('\r', '').replace('\n', '')
-  doPublish(topic, payload)
+  return payload
 
 
 def ramFree():
   raminfo = getRAMinfo()
-  ramfree = int(raminfo[2])
-  ramfreemb = ramfree / 1024
-  topic = MQTT_CLIENT_CODE + "/pimemfree/sensor/update"
-  payload = '{"value":' + str(ramfreemb) + '}'
-  doPublish(topic, payload)
+  total = int(raminfo[0])
+  free = int(raminfo[2])
+  used =  round( (total - free) / total * 100, 2 )
+  freePerc = float(100) - float(used)
+  payload = '{"value":' + str(freePerc) + '}'
+  return payload
 
- 
-def doPublish(topic, payload):
+next_reading = time.time()
 
-  tls = {
-    'cert_reqs' : ssl.CERT_REQUIRED,
-    'tls_version' : ssl.PROTOCOL_TLSv1_2
-  }
+client = mqtt.Client(client_id=MQTT_CLIENT_ID, clean_session=True, userdata=None, transport="tcp")
+client.username_pw_set(MQTT_USER, MQTT_PASS)
+client.tls_set (cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2)
+client.tls_insecure_set (False)
+client.connect(MQTT_SERVER, MQTT_PORT, 60)
 
-  publish.single(
-    topic=topic,
-    payload=payload,
-    hostname=MQTT_SERVER,
-    port=MQTT_PORT,
-    retain=False,
-    auth=MQTT_USER_PASS,
-    qos=0,
-    tls=tls,
-    client_id=MQTT_CLIENT_ID
-  )
+client.loop_start()
 
-threading.Thread(target=lambda: every(60, cpuTemp)).start()
-threading.Thread(target=lambda: every(60, diskUsed)).start()
-threading.Thread(target=lambda: every(60, ramFree)).start()
+try:
+  while True:
+    # CPU Temperature
+    topic = MQTT_CLIENT_CODE + "/picputemp/sensor/update"
+    payload = cpuTemp()
+    client.publish(topic, payload, qos=0, retain=False)
+    time.sleep(0.02)
+
+    # Disk Used Percentage
+    topic = MQTT_CLIENT_CODE + "/pidiskused/sensor/update"
+    payload = diskUsed()
+    client.publish(topic, payload, qos=0, retain=False)
+    time.sleep(0.02)
+
+    # Free Memory MB
+    topic = MQTT_CLIENT_CODE + "/pimemfree/sensor/update"
+    payload = ramFree()
+    client.publish(topic, payload, qos=0, retain=False)
+
+    # Publish sensors every 60 seconds
+    next_reading += 60
+    sleep_time = next_reading-time.time()
+    if sleep_time > 0:
+      time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
+client.disconnect()
+
